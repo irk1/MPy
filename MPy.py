43bf755fc992fc45fa65426b32033e5a1e21510a
irk1 <Izzy.kriegsman@gmail.com>
2023-01-31 12:40:11 -0500

    Update MPy.py

--- MPy.py
+++ MPy.py
@@ -1,13 +1,13 @@
-import cv2
-video_capture = cv2.VideoCapture(0)
-while True:
-    if not video_capture.isOpened():
+import cv2 #downloads stuffs
+video_capture = cv2.VideoCapture(0) #hijacks webcam
+while True: #stuffs go brrrrr
+    if not video_capture.isOpened(): #makes stuffs stop brrrr
         print('Unable to load camera.')
         sleep(5)
         pass
-    ret, frame = video_capture.read()
+    ret, frame = video_capture.read() #yoinks images from video
 
-    cv2.imshow('Video', frame,)
+    cv2.imshow('Video', frame,)#converts the yoinked frames into pretty pictures and displays them
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
